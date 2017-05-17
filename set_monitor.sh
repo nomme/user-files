acfc1fd8704881a544f27eb388fcaea16adf3f1c
Jimmie Hogklint <jimmie.hogklint@delphi.com>
2017-05-17 14:09:33 +0200

    Added some files

---
+++ set_monitor.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+INTERNAL_MONITOR="eDP1"
+DP1="DP1-1"
+DP2="DP1-2"
+HDMI1="HDMI1"
+
+CONNECTED_MONITORS="$(xrandr | grep -E "\sconnected" | cut -d' ' -f1)"
+
+function is_monitor()
+{
+    echo "$CONNECTED_MONITORS" | grep $1
+}
+
+if [ -n "$(is_monitor $DP1)" ] && [ -n "$(is_monitor $DP2)" ]
+then
+    xrandr --output $DP1 --auto --pos 0x280 --output $DP2 --auto --rotate left --pos 2560x0 --output $INTERNAL_MONITOR --off
+    echo "Dual: $DP1, $DP2"
+elif [ -n "$(is_monitor $DP1)" ] && [ -n "$(is_monitor $INTERNAL_MONITOR)" ]
+then
+    xrandr --output $DP1 --auto --output $INTERNAL_MONITOR --auto --left-of $DP1
+    echo "Dual: $INTERNAL_MONITOR, $DP1"
+elif [ -n "$(is_monitor $HDMI1)" ] && [ -n "$(is_monitor $INTERNAL_MONITOR)" ]
+then
+    xrandr --output $HDMI1 --auto --output $INTERNAL_MONITOR --auto --left-of $HDMI1
+    echo "Dual: $INTERNAL_MONITOR, $HDMI1"
+elif [ -n "$(is_monitor $DP1)" ]
+then
+    xrandr --output $DP1 --auto
+    echo "Single: $DP1"
+elif [ -n "$(is_monitor $HDMI1)" ]
+then
+    xrandr --output $HDMI1 --auto
+    echo "Single: $HDMI1"
+elif [ -n "$(is_monitor $INTERNAL_MONITOR)" ]
+then
+    xrandr --output $INTERNAL_MONITOR --auto --output $DP1 --off --output $DP2 --output $HDMI1 --off
+    echo "Single: $INTERNAL_MONITOR"
+fi
+
