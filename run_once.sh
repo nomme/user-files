dde09d10f5a2c12acfaedb1bfdabeb7e53a7d5d0
Jimmie Hogklint <jimmie.hogklint@delphi.com>
2017-05-22 09:24:14 +0200

    Changes...

---
+++ run_once.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+
+function run_it()
+{
+    if [ -z "$(pgrep $1)" ]
+    then
+        $@ &
+    fi
+}
+
+run_it chromium --restore-last-session
+run_it pidgin
+run_it nm-applet
+#run_it pasystray
